1639cc1dc6ab0f9bdeaa219da45cba8f2b64a9c8
이은영 <ieun-yeong@ieun-yeong-ui-MacBookAir.local>
2025-07-08 11:39:06 +0900

    init: first commit

---
+++ src/components/FamilyTree.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
+
+const Tree = dynamic(() => import('react-d3-tree').then(mod => mod.Tree), {
+  ssr: false,
+});
+
+export default function FamilyTree() {
+  const [treeData, setTreeData] = useState(null);
+
+  useEffect(() => {
+    fetch('/data/family_tree.json')
+      .then((res) => res.json())
+      .then((data) => setTreeData(data));
+  }, []);
+
+  if (!treeData) return <div>Loading...</div>;
+
+  return (
+    <div style={{ width: '100%', height: '100vh' }}>
+      <Tree data={treeData} orientation="horizontal" pathFunc="step" initialDepth={2} 
+      />
+    </div>
+  )
+}
